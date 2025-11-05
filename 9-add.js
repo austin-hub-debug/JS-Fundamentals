9892c06f9339803b6214e26369056c19810a4430
austin-hub-debug <austinodongo8@gmail.com>
2025-11-06 00:10:58 +0300

    .\9-add.js

---
+++ 9-add.js
@@ -0,0 +1,10 @@
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+if (isNaN(a) && isNaN(b)) {
+    console.log('NaN');
+} else {
+function add (a, b) {
+  return a + b;
+}
+console.log(add(a, b));
+}
