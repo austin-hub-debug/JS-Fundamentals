a50ecacdedcba3e5051a82580f6c07de0c7f483f
austin-hub-debug <austinodongo8@gmail.com>
2025-11-05 23:43:26 +0300

    .\7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,13 @@
-const num = process.argv.slice(2);
+
+const num = process.argv[2];
 const number = Number(num);
+
 if (isNaN(number)) {
-  console.log("Missing number of occurrences");
-}else {
-    i=1;
-    while(i<=number){
-    console.log("C is fun");
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < number) {
+    console.log('C is fun');
     i++;
-}
+  }
 }
